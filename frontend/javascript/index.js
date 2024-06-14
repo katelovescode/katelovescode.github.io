ca9e15c1a52d44eb5621659eb630a15bdde6c729
Kate <kate@katelovescode.com>
2024-06-14 09:49:16 -0500

    Resetting the style changes so I can try the Bulma theme

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,15 +1,7 @@
-import "$styles/index.css";
-import "$styles/syntax-highlighting.css";
+import "$styles/index.css"
+import "$styles/syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
-import components from "$components/**/*.{js,jsx,js.rb,css}";
+import components from "$components/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!");
-
-const navbar = document.querySelector(".nav-container");
-const observer = new IntersectionObserver(
-  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-  { threshold: [1] }
-);
-
-observer.observe(navbar);
+console.info("Bridgetown is loaded!")
